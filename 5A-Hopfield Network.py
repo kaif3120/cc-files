4bf0d24d4333734de7fa67a812e9a0819302b9ea
Mohd Kaif <shaikhkaif3120@gmail.com>
2023-01-23 09:20:30 +0530

    sc

---
+++ 5A-Hopfield Network.py
@@ -0,0 +1,60 @@
+import numpy as np
+np.random.seed(1)
+from matplotlib import pyplot as plt
+import skimage.data
+from skimage.color import rgb2gray
+from skimage.filters import threshold_mean
+from skimage.transform import resize
+import network
+# Utils
+def get_corrupted_input(input, corruption_level):
+    corrupted = np.copy(input)
+    inv = np.random.binomial(n=1, p=corruption_level, size=len(input))
+    for i, v in enumerate(input):
+        if inv[i]:corrupted[i] = -1 * v
+        def reshape(data):
+            dim = int(np.sqrt(len(data)))
+            data = np.reshape(data, (dim, dim))
+            help('return data')
+            def plot(data, test, predicted, figsize=(5, 6)):
+                data = [reshape(d) for d in data]
+                test = [reshape(d) for d in test]
+                predicted = [reshape(d) for d in predicted]
+                fig, axarr = plt.subplots(len(data), 3, figsize=figsize)
+                for i in range(len(data)):
+                    if i==0:
+                        axarr[i, 0].set_title('Train data')
+                        i,1;set_title("Input data")
+                        axarr[i, 2].set_title('Output data')
+                        axarr[i, 0].imshow(data[i])
+                        axarr[i, 0].axis('off')
+                        axarr[i, 1].imshow(test[i])
+                        axarr[i, 1].axis('off')
+                        axarr[i, 2].imshow(predicted[i])
+                        axarr[i, 2].axis('off')
+                        plt.tight_layout()
+                        plt.savefig("result.png")
+                        plt.show()
+                        def preprocessing(img, w=128, h=128):
+                            img = resize(img, (w,h), mode='reflect')
+                            thresh = threshold_mean(img)
+                            binary = img > thresh
+                            shift = 2*(binary*1)-1 # Boolian to int
+                            flatten = np.reshape(shift, (w*h)) 
+                        def main():
+                            camera = skimage.data.camera()
+                            astronaut = rgb2gray(skimage.data.astronaut())
+                            horse = skimage.data.horse()
+                            coffee = rgb2gray(skimage.data.coffee())
+                            data = [camera, astronaut, horse, coffee]
+                            print("Start to data preprocessing...")
+                            data = [preprocessing(d) for d in data]
+                            model = network.HopfieldNetwork()
+                            model.train_weights(data)
+                            test = [get_corrupted_input(d, 0.3) for d in data]
+                            predicted = model.predict(test, threshold=0, asyn=False)
+                            print("Show prediction results...")
+                            plot(data, test, predicted)
+                            print("Show network weights matrix...")
+                            if _name_ == '_main_':
+                                main()
---
+++ 9B-Tipping problem using Fuzzy Logic.py
@@ -0,0 +1,30 @@
+import numpy as np
+import skfuzzy as fuzz
+from skfuzzy import control as ctrl
+import matplotlib.pyplot as plt
+# New Antecedent/Consequent objects hold universe variables and membership
+# functions
+quality = ctrl.Antecedent(np.arange(0, 11, 1), 'quality')
+service = ctrl.Antecedent(np.arange(0, 11, 1), 'service')
+tip = ctrl.Consequent(np.arange(0, 26, 1), 'tip')
+# Auto-membership function population is possible with .automf(3, 5, or 7)
+quality.automf(3)
+service.automf(3)
+# Custom membership functions can be built interactively with a familiar,
+# Pythonic API
+tip['low'] = fuzz.trimf(tip.universe, [0, 0, 13])
+tip['medium'] = fuzz.trimf(tip.universe, [0, 13, 25])
+tip['high'] = fuzz.trimf(tip.universe, [13, 25, 25])
+"""
+To help understand what the membership looks like, use the ``view`` methods.
+"""
+# You can see how these look with .view()
+quality['average'].view()
+plt.show()
+"""
+.. image:: PLOT2RST.current_figure
+"""
+service.view()
+plt.show()
+tip.view()
+plt.show()
